41a1931a3a495cb9a4c0f6b39d3c18e447fd687f
Sansone11 <sansonemirco@gmail.com>
2022-10-19 21:27:36 +0200

    view-edit

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,12 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')
+    ->prefix('admin')
+    ->name('admin.')
+    ->namespace('Admin')
+    ->group(function () {
+        Route::get('/home', 'HomeController@index')->name('admin.home');
+        Route::resource('posts', 'PostController');
+    });
